2dbd1f9545b6421a5dbc2e5124d985d7f9d0883f
inceserhatabdullah <inceserhatabdullah@gmail.com>
2022-07-03 23:34:33 +0300

    responsive design bug&fix and others.

--- src/components/about/feed.js
+++ src/components/about/feed.js
@@ -1,29 +1,29 @@
-import { properties, toUpperCaseFirstCase } from "../../configuration/config";
+import {properties, toUpperCaseFirstCase} from "../../configuration/config";
 
 const aboutDatas = [
-  {
-    icon: properties.icons.experience,
-    h5: properties.menus.experience,
-    title: `${
-      properties.experienceYear
-    } years ${toUpperCaseFirstCase(properties.menus.experience)}`,
-  },
+    {
+        icon: properties.icons.experience,
+        h5: properties.menus.experience,
+        title: `${
+            properties.experienceYear
+        } years ${toUpperCaseFirstCase(properties.menus.experience)}`,
+    },
 
-  {
-    icon: properties.icons.projects,
-    h5: properties.menus.projects,
-    title: `${properties.projectCount}+ Completed`,
-  },
+    {
+        icon: properties.icons.projects,
+        h5: properties.menus.projects,
+        title: `${properties.projectCount}+ Completed`,
+    },
 ];
 
 const aboutParagraph = (
-  <p>
-    Hello guys. <br></br> <br></br>I'm <strong> Abdullah Serhat Ince </strong>
-    and 25 years old. <br></br>My hobbies include writing code, watching writers
-    and seeing your success, and trying new technologies, watching video games,
-    and reading different kinds of books and other than that, I try to take care
-    of my health with sports and regular nutrition.
-  </p>
+    <p>
+        Hello guys. &#128075; <br></br> <br></br> I'm <strong> Abdullah Serhat Ince </strong>
+        and 25 years old. <br></br> <br></br> My hobbies include writing code, watching writers
+        and seeing your success. <br></br>Trying new technologies, watching video games,
+        and reading different kinds of books. <br></br><br></br> Other than that, I try to take care
+        of my health with sports and regular nutrition.
+    </p>
 );
 
-export { aboutDatas, aboutParagraph };
+export {aboutDatas, aboutParagraph};
